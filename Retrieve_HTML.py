2cf62a155eb11ee503003b7cafb782abcba7c1fc
Michael Gregor <michael@ferrossystems.com>
2017-02-19 11:28:41 -0800

    Threaded the calls and made improvements

--- Retrieve_HTML.py
+++ Retrieve_HTML.py
@@ -1,55 +1,56 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-import json
+from threading import Thread
+from queue import Queue
 
 
-def website_feeder(website_name, web_url):
+def website_feeder(website_name, web_url, word):
 
     result = requests.get(web_url)
     soup = BeautifulSoup(result.text, 'html.parser')
     all_links = []
-    outside_links = []
     instances = []
+    failed_urls = []
 
     for link in soup.find_all('a'):
         all_links.append(link.get('href'))
 
     all_links = set(all_links)
-
+    queue = Queue()
     for link in all_links:
+        worker = Worker(queue)  # so that essentially all URLS are called
+        worker.daemon = True  # at the same tim
+        worker.start()
         if link is None:
             continue
         if "http" in link:
-            outside_links.append(link)
-            continue
-        elif "//" in link:
-            outside_links.append(link[2:len(link)])
+            queue.put((instances, link, word))
+        elif "//" == link[0:2]:
+            url = "http://" + link[2:len(link)]
+            queue.put((instances, url, word))
         else:
             url = web_url + link
-            web_site = requests.get(url)
-            if web_site.status_code == 200:
-                web_text = web_site.text
-                words = len([m.start() for m in re.finditer("Trump", web_text)])
-                instances.append({url: words})
-    '''
-    for link in outside_links:
-        if "www" not in link:
-            url = "http://www.{}".format(link)
-        elif "http://" not in link:
-            url = "http://{}".format(link)
+            queue.put((instances, url, word))
 
+    queue.join()
+    total_mentions = 0
+    for item in instances:
+        for value in item.values():
+            total_mentions += value
+    print("{} is mentioned {} times on {} across {} links found on the home page".format(word, total_mentions, website_name, len(instances)))
+
+
+def search_site(instances, url, word):
+    try:
         web_site = requests.get(url)
-        if web_site.status_code == 200:
-            web_text = web_site.text
-            words = len([m.start() for m in re.finditer("Trump", web_text)])
-            instances.append({url: words})
-    '''
-    with open("{}.txt".format(website_name), "w") as f:
-        f.write("Number of sites is {}\n".format(len(instances)))
-        for item in instances:
-            f.write(json.dumps(item))
-            f.write("\n")
+    except:
+        return
+    if web_site.status_code == 200:
+        web_text = web_site.text
+        words = len([m.start() for m in re.finditer(word, web_text)])
+        instances.append({url: words})
+
 
 def main():
 
@@ -57,10 +58,28 @@ def main():
         "CNN": "http://www.cnn.com",
         "MSNBC": "http://www.msnbc.com",
         "Fox News": "http://www.foxnews.com",
+        "Wall Street Journal": "http://www.wsj.com/",
+        "Foreign Affaris": "http://www.foreignaffairs.com/",
+        "The Moscow Times": "http://themoscowtimes.com/",
+        "Zeit Online": "http://www.zeit.de/index"
     }
+    words = ["Trump", "Russia"]
+    for word in words:
+        for key, value in website_list.items():
+            website_feeder(key, value, word)
 
-    for key, value in website_list.items():
-        website_feeder(key, value)
+
+class Worker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            instances, url, word = self.queue.get()
+            search_site(instances, url, word)
+            self.queue.task_done()
 
 if __name__ == "__main__":
     main()
